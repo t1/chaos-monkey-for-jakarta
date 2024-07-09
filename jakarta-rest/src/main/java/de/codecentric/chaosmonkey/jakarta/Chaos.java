65615dc31f158af2ceb5bbe6020597a2453e4305
Ruediger zu Dohna <snackbox@sinntr.eu>
2024-07-09 07:15:44 +0200

    use `volatile` instead of `transient` (stupid me)

--- jakarta-rest/src/main/java/de/codecentric/chaosmonkey/jakarta/Chaos.java
+++ jakarta-rest/src/main/java/de/codecentric/chaosmonkey/jakarta/Chaos.java
@@ -1,14 +1,6 @@
 package de.codecentric.chaosmonkey.jakarta;
 
 import jakarta.json.bind.annotation.JsonbTransient;
-import jakarta.json.bind.annotation.JsonbTypeDeserializer;
-import jakarta.json.bind.annotation.JsonbTypeSerializer;
-import jakarta.json.bind.serializer.DeserializationContext;
-import jakarta.json.bind.serializer.JsonbDeserializer;
-import jakarta.json.bind.serializer.JsonbSerializer;
-import jakarta.json.bind.serializer.SerializationContext;
-import jakarta.json.stream.JsonGenerator;
-import jakarta.json.stream.JsonParser;
 import jakarta.ws.rs.core.Response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,22 +9,17 @@ import lombok.NoArgsConstructor;
 import lombok.With;
 import lombok.experimental.Accessors;
 
-import java.lang.reflect.Type;
 import java.time.Duration;
 import java.util.function.Consumer;
 
 import static de.codecentric.chaosmonkey.jakarta.ChaosEvent.Type.APPLY;
-import static jakarta.json.stream.JsonParser.Event.KEY_NAME;
 import static jakarta.ws.rs.core.Response.StatusType;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder @With @Accessors(chain = true)
-// TODO is there an easier way to do this?
-@JsonbTypeSerializer(Chaos.Serializer.class)
-@JsonbTypeDeserializer(Chaos.Deserializer.class)
 public class Chaos {
-    private transient Integer failureCount;
-    private transient Integer statusCode;
-    private transient Integer delay;
+    private volatile Integer failureCount;
+    private volatile Integer statusCode;
+    private volatile Integer delay;
 
     public boolean active() {return failureCount() > 0 && (statusCode != null || delay != null);}
 
@@ -75,7 +62,9 @@ public class Chaos {
 
     private boolean nextFailure() {
         if (!active()) return false;
-        --failureCount;
+        synchronized (this) {
+            --failureCount;
+        }
         return true;
     }
 
@@ -94,33 +83,4 @@ public class Chaos {
         abort.accept(Response.status(failureStatus).build());
     }
 
-    public static class Serializer implements JsonbSerializer<Chaos> {
-        @Override public void serialize(Chaos obj, JsonGenerator generator, SerializationContext ctx) {
-            generator.writeStartObject();
-            if (obj.failureCount != null) ctx.serialize("failureCount", obj.failureCount, generator);
-            if (obj.statusCode != null) ctx.serialize("statusCode", obj.statusCode, generator);
-            if (obj.delay != null) ctx.serialize("delay", obj.delay, generator);
-            generator.writeEnd();
-        }
-    }
-
-    public static class Deserializer implements JsonbDeserializer<Chaos> {
-        @Override public Chaos deserialize(JsonParser parser, DeserializationContext ctx, Type rtType) {
-            var chaos = new Chaos();
-            while (parser.hasNext()) {
-                var event = parser.next();
-                if (event == KEY_NAME) {
-                    var fieldName = parser.getString();
-                    parser.next();
-                    var value = ctx.deserialize(Integer.class, parser);
-                    switch (fieldName) {
-                        case "failureCount" -> chaos.failureCount = value;
-                        case "statusCode" -> chaos.statusCode = value;
-                        case "delay" -> chaos.delay = value;
-                    }
-                }
-            }
-            return chaos;
-        }
-    }
 }
